9aa81386fa723e6f084ff7f3557146390981aecd
Frida <fridajkb@gmail.com>
2025-05-22 15:58:59 +0200

    stuff Co-authored-by: bong1234sjong <bong1234sjong@users.noreply.github.com>

--- test.py
+++ test.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 def display(movie1_title="Movie 00", movie2_title="Movie 2"):
 
 
-    return render_template('testhtml.html', movie1_title="Movie 00", movie2_title="Movie 2")
+    return render_template('testhtml.html', movie1_title="Movie 00", movie2_title="Movie 2", result=False)
 
 @app.route('/check_answer', methods=['POST'])
-def check_answer(answer):
+def check_answer():
     m1 = request.form["movie1_title"]
     m2 = request.form["movie2_title"]
     choice = request.form["choice"]        # "higher" or "lower"
-    is_correct = check_answer(m1, m2, choice)
+    is_correct = True
 
     return render_template("testhtml.html",
-                           movie1_title=next1,
-                           movie2_title=next2,
-                           result="Correct!" if is_correct else "Incorrect.")
+                           movie1_title="movie1_title",
+                           movie2_title="movie2_title",
+                           result=is_correct)
 
 
 @app.route("/next")
